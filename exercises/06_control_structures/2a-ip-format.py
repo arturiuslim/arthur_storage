f81a0fa5ac36fe9dc926e66eed451d042d352c50
arturius <arturiuslim@gmail.com>
2020-10-17 16:32:04 +0200

    fixin bugs in 2a-ip-format.py

--- exercises/06_control_structures/2a-ip-format.py
+++ exercises/06_control_structures/2a-ip-format.py
@@ -7,22 +7,27 @@ addr = raw_input('Please type an ip address: ')
 ip = addr.split('.')
 #print(ip[0])
 ip_ok = False
+octet_ok = False
 
 while not ip_ok:
-	if len(ip) != 4 and '.' not in addr :
+	if len(ip) == 4 and '.' in addr :
+		ip_ok = True
+	else:
 		print('Wrong lenght!')
 		addr = raw_input('Type ip again: ')
 		ip = addr.split('.')
-	else:
-		ip_ok = True
-
+while not octet_ok:
 	for i in range(len(ip)):
-        	if int(ip[i]) not in range(256):
-			ip_ok = False	
-			print('Out of range')
-			addr = raw_input('Type ip address: ')
+		if ip[i].isdigit() and int(ip[i]) in range(256):
+			octet_ok = True
+		else:	
+			octet_ok = False
+			
+			addr = raw_input('WRONG! Type ip address: ')
 			ip = addr.split('.')
-print('Length and format are ok')
+if ip_ok and octet_ok:
+	print(len(ip))
+	print('Length and format are ok')
 
 						       
 
