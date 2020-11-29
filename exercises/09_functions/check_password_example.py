eeedc228e1f386213142828fdc5b0929e0603b18
arturiuslim <arturiuslim@gmail.com>
2020-11-29 07:36:39 +0100

    Start working with pytest

---
+++ exercises/09_functions/check_password_example.py
@@ -0,0 +1,13 @@
+def check_password(username, password, password_length=8,
+		   check_username=True, spec_sym=True):
+	if len(password) < password_length:
+		return False
+	elif check_username and username in password:
+		return False
+	else:
+		return True
+	
+def test_check_password_func():
+	assert check_password('user', '12345678') == True, "For user USER this password is correct"
+	assert check_password('user', '12345678', password_length=10) == False
+	assert check_password('user', '12345678user') == False
