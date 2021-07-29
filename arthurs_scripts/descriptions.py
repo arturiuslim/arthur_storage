231215b932155ee05abc805911f0f2988ffe6769
arturius <arturiuslim@gmail.com>
2021-07-29 14:05:24 +0200

    modified script for interface description

--- arthurs_scripts/descriptions.py
+++ arthurs_scripts/descriptions.py
@@ -6,14 +6,14 @@ in1 = input('Etner 1-st interface: ')
 in1 = int(in1)
 in2 = input('Enter last interface: ')
 in2 = int(in2)
-interface_template = [ 
-			'set interfaces xe-0/0/{in1} description{f} ']
+interface_template = 'set interfaces xe-0/0/{in1} description{f}'
 			
 print(in1, in2)
 i = in1
 int(i)
-with open(i_file) as f:
-	for lines in f:
-		for i in range(in2):
-			for template in interface_template:
-				print(template.format(in1=i, f=lines))
+with open(i_file, 'r') as f:
+	for i in range(in2):
+		for lines in f:
+#			for template in interface_template:
+			print(interface_template.format(in1=i, f=lines))
+#			print(lines)
