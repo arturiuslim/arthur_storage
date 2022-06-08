e0d61aeb3561dd39bdc81cd8439c6434e79d9b6a
arturiuslim <arturiuslim@gmail.com>
2022-06-08 14:50:55 +0200

    csv to dict added, 4 random items from dict

--- arthurs_scripts/parse_csv.py
+++ arthurs_scripts/parse_csv.py
@@ -1,10 +1,18 @@
 import csv
+import random
 
 svensk = {}
 with open('dict.csv', 'r') as csv_file:
 #	csv_reader = csv.DictReader(csv_file)
 	svensk = dict(filter(None, csv.reader(csv_file)))
-	print(svensk.keys())
+#	print(svensk.keys())
 #	for line in csv_reader:
 #		print(line)
+so = [random.choice(list(svensk)) for i in range(4)]
+ro = []
+for i in so:
+	ro.append(svensk.get(i))
 
+tmp_dict = dict(zip(so, ro)) 
+print([random.choice(list(tmp_dict))])
+print(tmp_dict.values())
