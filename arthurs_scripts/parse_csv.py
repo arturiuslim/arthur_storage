448a202e2099a09ae30f2dd2707351a9b10f1e2c
arturiuslim <arturiuslim@gmail.com>
2022-06-10 14:05:37 +0200

    random function added

--- arthurs_scripts/parse_csv.py
+++ arthurs_scripts/parse_csv.py
@@ -7,12 +7,24 @@ svensk = {}
 with open('dict.csv', 'r') as csv_file:
 	svensk = dict(filter(None, csv.reader(csv_file)))
 
-so = [random.choice(list(svensk)) for i in range(4)]
-ro = []
-for i in so:
-	ro.append(svensk.get(i))
-tmp_dict = dict(zip(so, ro)) 
-nyord = [random.choice(list(tmp_dict))]
+#so = [random.choice(list(svensk)) for i in range(4)]
+#ro = []
+#for i in so:
+#	ro.append(svensk.get(i))
+#tmp_dict = dict(zip(so, ro)) 
+#nyord = [random.choice(list(tmp_dict))]
+#print(nyord)
+
+def tmpdict(indict):
+	loc_so = [random.choice(list(indict)) for i in range(4)]
+	loc_ro = []
+	for j in loc_so:
+		loc_ro.append(indict.get(j))
+	loc_dict = dict(zip(loc_so, loc_ro))
+	nyord_loc = [random.choice(list(loc_dict))]
+	return (loc_dict, loc_ro, nyord_loc)
+
+newdict,ro,nyord = tmpdict(svensk)
 print(nyord)
 
 options = [ro[0], ro[1], ro[2], ro[3], "[q] Quit"]
@@ -27,6 +39,7 @@ while quitting == False:
 		quitting = True
 	else:
 		if(optionsChoice == svensk.get(nyord[0])):
+			print(optionsChoice)
 			print("det stämmer")
 		else:
 			print("det fel")
