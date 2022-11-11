8d1f87f37a4cbdb65ef147df67b62a9ac9903484
arturiuslim <arturiuslim@gmail.com>
2022-11-11 21:11:02 +0100

    Lection 12 regexp.

---
+++ examples/15_module_re/ex01_parse_log.py
@@ -0,0 +1,14 @@
+import re
+
+regex = r"Host (\S+) .+ port (\S+) and port (\S+)"
+
+ports = set()
+
+with open("log.txt") as f:
+	for line in f:
+		match = re.search(regex, line)
+		if match:
+			print(match.group())
+			print(match.group(2, 3))
+			ports.update(match.group(2, 3))
+print(ports)
