5458a5f40323bf20fee649e3e2fe5a67f6bc5660
arturiuslim <arturiuslim@gmail.com>
2023-01-28 12:18:49 +0100

    task_15_1b completed, need to modify

--- exercises/15_module_re/exercise_15_1b.py
+++ exercises/15_module_re/exercise_15_1b.py
@@ -5,30 +5,25 @@ from pprint import pprint
 from sys import argv
 
 def get_ip_from_cfg(infl):
-#	regexp = r"([.\d]+)"
 	regexp = (
-#		r"interface [\S+]+\n"
-		r"interface (?P<interface>[\S+]+)\n"
-		r".*?"
-#		r"(?P<ip> ip address (\d+\.\d+\.\d+\.\d+))"
-		r"((\d+\.\d+\.\d+\.\d+ \d+\.\d+\.\d+\.\d+))\n+"
-	)
+		r"(interface [\S+]+)|(ip address \d+\.\d+\.\d+\.\d+ \d+\.\d+\.\d+\.\d+)"
+		)
 	result_list = []
-	m_all = re.findall(regexp, infl, re.DOTALL)
-	print(m_all)
-	for m  in m_all:
-#		print(m[1])
-		result_list.append(m[0])
-		result_list.append(m[1])
-#		result_list.append(group_tp)
+	with open(infl) as f:
+		for line in f:
+			match = re.search(regexp, line)
+			if match:
+#				print(match.groups())
+				if match.group(1) != None:
+					result_list.append(match.group(1))
+				if match.group(2) != None:
+					result_list.append(match.group(2))
 	return result_list
 
 def main():
 	in_fl = argv[1]
-	with open (in_fl, "r") as f:
-		content = f.read()
-		pprint(get_ip_from_cfg(content))
-
+	output = get_ip_from_cfg(in_fl)
+	pprint(output)
 
 if __name__ == "__main__":
 	main()
