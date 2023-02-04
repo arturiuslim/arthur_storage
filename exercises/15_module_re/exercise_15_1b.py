5781faa70baaafd01a1f849ae91afd128cb38a4b
arturiuslim <arturiuslim@gmail.com>
2023-02-04 09:36:47 +0100

    exercise_15_1b.py finished

--- exercises/15_module_re/exercise_15_1b.py
+++ exercises/15_module_re/exercise_15_1b.py
@@ -7,37 +7,39 @@ from sys import argv
 def get_ip_from_cfg(infl):
 	regexp = (
 		r"(interface [\S+]+)|(ip address \d+\.\d+\.\d+\.\d+ \d+\.\d+\.\d+\.\d+)" #////working one
-#			r"interface (?P<interface>[\S+]+)|ip address (?P<ip>\d+\.\d+\.\d+\.\d+ \d+\.\d+\.\d+\.\d+)"
+#		r"interface (?P<interface>[\S+]+)|ip address (?P<ip>\d+\.\d+\.\d+\.\d+ \d+\.\d+\.\d+\.\d+)"
 		)
 	result_list = []
+#	result_list = {}
 	with open(infl) as f:
 		for line in f:
 			match = re.search(regexp, line)
 			if match:
 				"""
+				### Doesn't work because of lastgroup does not catch 2 IPs
 				group = match.lastgroup
 				value = match.group(group)
 				print(group)
 				print(value)
 				if group == "interface":
-					print(group)
+#					print(group)
 					result_list[value] = {}
 					interface = value
 				elif group == "ip":
 					tmp = value.split()
 					value = tuple(tmp)
 					result_list[interface]["ip"] = value
-					print(group)
+#					print(group)
 
-					print(type(value))
+#					print(type(value))
 				"""
 #//////////	Works as a list
-#				print(match.groups())
+#				print(type(match.groups()))
 				if match.group(1) != None:
 					result_list.append(match.group(1))
 				elif match.group(2) != None:
-					result_list.append(match.group(2))
-#					result_list.append(match.group(3))
+					tmp = match.group(2).split()
+					result_list.append(tuple(tmp))
 #/////////////////////////////////////////////////////////////////////////////////
 	return result_list
 
